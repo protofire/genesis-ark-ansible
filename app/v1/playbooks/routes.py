68f91edd93beb4a27f45643b6cf7ae9676a96c87
Ales Dumikau <ales.dumikau@protofire.io>
2024-04-09 14:37:31 +0300

    [feat] - get extra_vars and tags from json

--- app/v1/playbooks/routes.py
+++ app/v1/playbooks/routes.py
@@ -1,4 +1,4 @@
-from flask import jsonify
+from flask import jsonify, request
 
 from app.v1.playbooks import playbooks_bp
 from app.v1.playbooks.models import Playbook
@@ -11,7 +11,11 @@ def handle_list_playbooks():
 
 @playbooks_bp.route("/<playbook_name>/run", methods=["POST"])
 def handle_run_playbook(playbook_name):
+    payload = request.json
+    extra_vars = payload.get("extra_vars", None)
+    tags = payload.get("tags", None)
+
     playbook = Playbook(playbook_name=playbook_name, project_id="hello")
-    job_id = playbook.run()
+    job_id = playbook.run(extra_vars=extra_vars, tags=tags)
 
     return jsonify({"job_id": job_id}), 200
